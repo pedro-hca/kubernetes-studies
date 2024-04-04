c78c08ac80e2ca6d862f79f581e86ca2894f4fc8
pedro-hca <pedroandradecontato@hotmail.com>
2024-04-03 23:41:03 -0300

    feat: add configmap

--- server.go
+++ server.go
@@ -1,12 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Kubernetes!!!</h1>"))
+	// w.Write([]byte("<h1>Hello Kubernetes!!!</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/myTasks/task.txt")
+	if err != nil {
+		log.Fatalf("Error reading file", err)
+	}
+	fmt.Fprintf(w, "Todo: %s.", string(data))
 }
