d227118c704c0358aeb69602abd2eb13b489144e
pedro-hca <pedroandradecontato@hotmail.com>
2024-04-07 00:14:36 -0300

    add readiness liveness and secret

--- server.go
+++ server.go
@@ -6,10 +6,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
@@ -20,10 +25,26 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
 }
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password: %s.", user, password)
+}
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	data, err := ioutil.ReadFile("/go/myTasks/task.txt")
 	if err != nil {
-		log.Fatalf("Error reading file", err)
+		log.Fatalf("Error reading file: %v", err)
 	}
 	fmt.Fprintf(w, "Todo: %s.", string(data))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() > 100 {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	}
 }
